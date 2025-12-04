b3f2c0fc63cd742225656cdd4a0c8a265958ca6f
beamimc <beatrizcampillo29@gmail.com>
2025-12-03 23:39:50 -0500

    include time and calories analysis

--- author_analysis.r
+++ author_analysis.r
@@ -6,8 +6,6 @@ library(plotly)
 
 # load data
 df <- readr::read_csv("data/clean_cuisines.csv")
-
-
 # Which authors are most successful: who is most prolific, who has the highest average ratings or popularity,
 # and do top authors specialize by cuisine, ingredient, or recipe length?
 
@@ -80,43 +78,14 @@ combo <- patchwork::wrap_plots(plots, ncol = 1)
 ggsave("figures/author_success.png", combo, width = 20, height = 20)
 
 
-# make interactive plot with plotly for all authors
-all_authors <- df %>%
-  group_by(author) %>%  
-  summarise(num_recipes = n(),
-            avg_rating = mean(avg_rating, na.rm = TRUE),
-            num_ratings = mean(total_ratings, na.rm = TRUE)) %>%
-  arrange(desc(num_recipes)) |>
-    mutate( 
-        #most_contrib = TRUE if more than 10 recipes
-        most_contrib = ifelse(num_recipes > 10, TRUE, FALSE),
-        #top_rated = TRUE if in the top 50 by avg_rating
-        top_rated = ifelse(avg_rating >= 4.4, TRUE, FALSE)
-    )
-
-p_interactive <- ggplot(all_authors, aes(x = num_recipes, y = avg_rating, 
-                    label = author, text = paste("Author:", author, "<br>Number of Recipes:", 
-                                    num_recipes, "<br>Average Rating:", round(avg_rating, 2)))) +
-  geom_point(aes(color = most_contrib & top_rated), size = 3, alpha = 0.7) +
-  labs(title = "Author Success: Number of Recipes vs Average Rating",
-       x = "Number of Recipes",
-       y = "Average Rating",
-       color = "Top Contributor & Top Rated") +
-  theme_classic()   
-
-# ggplotly(p_interactive, tooltip = "text")
-# # save interactive plot
-# htmlwidgets::saveWidget(ggplotly(p_interactive, tooltip = "text"), "figures/author_success_interactive.html")
-
-
 # check type of food cuisines by these top authors
 top_author_cuisines <- df %>%
   filter(author %in% super_top_authors$author) %>%
-    group_by(author, country) %>%
+    group_by(author, region) %>%
     summarise(num_recipes = n()) %>%
     arrange(author, desc(num_recipes))
 # plot stacked bar chart of top authors and their cuisines
-p <- ggplot(top_author_cuisines, aes(x = author, y = num_recipes, fill = country)) +
+p <- ggplot(top_author_cuisines, aes(x = author, y = num_recipes, fill = region)) +
   geom_bar(stat = "identity") +
   labs(title = "Top Authors by Cuisine Type",
        x = "Author",
@@ -128,7 +97,36 @@ p <- ggplot(top_author_cuisines, aes(x = author, y = num_recipes, fill = country
 #save plot
 ggsave("figures/top_authors_cuisines.png", p, width = 12 , height = 8)
 
-# p_interactive <- ggplotly(p)
-# #save interactive plot
-# htmlwidgets::saveWidget(p_interactive, "figures/top_authors_cuisines_interactive.html")
+# check total_time by these top authors
+top_author_time <- df %>%
+  filter(author %in% super_top_authors$author) 
+
+p <- ggplot(top_author_time, aes(x = author, y = total_time, fill = author)) +
+  geom_boxplot() +
+  labs(title = "Total Time Distribution by Top Authors",
+        x = "Author", 
+        y = "Total Time (minutes)") +
+  theme_classic() +
+    theme(text = element_text(size = 16),
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          legend.position = "none")
+ggsave("figures/top_authors_total_time.png", p, width = 12 , height = 8)
+
+
+# check calories by these top authors
+top_author_calories <- df %>%
+  filter(author %in% super_top_authors$author)    
+# plot boxplot of number of ingredients by top authors
+
+p <- ggplot(top_author_calories, aes(x = author, y = calories, fill = author)) +
+  geom_boxplot() +
+  labs(title = "Calories Distribution by Top Authors",
+        x = "Author", 
+        y = "Calories") +
+  theme_classic() +
+    theme(text = element_text(size = 16),
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          legend.position = "none")
+
+ggsave("figures/top_authors_calories.png", p, width = 12 , height = 8)
 
